d66a1748083f48154ac644533b941908df77fe1e
LosEagle <6146756+LosEagle@users.noreply.github.com>
2021-03-07 13:10:37 +0100

    refactor: add hello world service worker

--- src/index.js
+++ src/index.js
@@ -5,5 +5,11 @@ import './styles.css';
 import './styles.styl';
 import App from './App';
 
+const registerServiceWorker = () =>
+  'serviceWorker' in navigator &&
+    navigator.serviceWorker.register('/service-worker.js');
+
+registerServiceWorker();
+
 const appContainer = document.getElementById('app');
 ReactDOM.render(<App />, appContainer);
